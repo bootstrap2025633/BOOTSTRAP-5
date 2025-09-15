fd11ff35d769f2866ea61e52f256fd797b89d379
bootstrap2025633 <abdullashadu7@gmail.com>
2025-09-15 07:19:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 if (!navigator.onLine) {
-  document.body.innerHTML = "<div style='display:flex;flex-direction:column;align-items:center;justify-content:center;height:100vh;background:#111;color:#fff;text-align:center;padding:20px;'><h1>Please connect to the internet</h1><p>This page needs internet to load styles and scripts.</p></div>";
+  document.body.innerHTML = "<div style='display:flex;flex-direction:column;align-items:center;justify-content:center;height:100vh;background:#111;color:#fff;text-align:center;padding:20px;'><h1>Please connect to the internet</h1><p>This page needs internet to load styles and scripts from GitHub.</p></div>";
 } else {
-  // After 3 seconds, go to home page file (same folder)
+  // After 3 seconds, go to home page file
   setTimeout(function () {
-    window.location.href = "home1.html";
+    window.location.href = 'home.html';
   }, 3000);
 }
